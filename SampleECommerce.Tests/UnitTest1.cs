c036d8f6328367e3e08d01d18350e098e6171c09
Rex Ng <rexcfnghk@gmail.com>
2024-07-20 12:38:29 +0800

    Initial

---
+++ SampleECommerce.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace SampleECommerce.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ SampleECommerce.Web/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleECommerce.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController
+{
+    [HttpGet("[action]")]
+    public ActionResult<string> Test()
+    {
+        return "hi";
+    }
+}
