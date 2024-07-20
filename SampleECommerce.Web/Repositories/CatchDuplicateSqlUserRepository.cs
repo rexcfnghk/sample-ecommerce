e8c981bd167e23359e8974280dc11b66b5498c22
Rex Ng <rexcfnghk@gmail.com>
2024-07-20 23:21:48 +0800

    Remove untestable code

--- SampleECommerce.Web/Repositories/CatchDuplicateSqlUserRepository.cs
+++ SampleECommerce.Web/Repositories/CatchDuplicateSqlUserRepository.cs
@@ -1,18 +1,16 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using JetBrains.Annotations;
+using Microsoft.Data.SqlClient;
 using SampleECommerce.Web.Exceptions;
 using SampleECommerce.Web.Services;
 
 namespace SampleECommerce.Web.Repositories;
 
-public class CatchDuplicateSqlUserRepository : IUserRepository
+[UsedImplicitly]
+public class CatchDuplicateSqlUserRepository(IUserRepository decoratee)
+    : IUserRepository
 {
     private const int UniqueConstraintViolationErrorNumber = 2627;
-    private readonly IUserRepository _decoratee;
 
-    public CatchDuplicateSqlUserRepository(IUserRepository decoratee)
-    {
-        _decoratee = decoratee;
-    }
     public async Task AddUserAsync(
         string userName,
         byte[] encryptedPassword,
@@ -21,7 +19,7 @@ public class CatchDuplicateSqlUserRepository : IUserRepository
     {
         try
         {
-            await _decoratee.AddUserAsync(
+            await decoratee.AddUserAsync(
                 userName,
                 encryptedPassword,
                 salt,
@@ -31,7 +29,7 @@ public class CatchDuplicateSqlUserRepository : IUserRepository
         {
             throw new DuplicateUserNameException(
                 userName,
-                $@"A user with the username {userName} already exists.",
+                $"A user with the username {userName} already exists.",
                 e);
         }
     }
