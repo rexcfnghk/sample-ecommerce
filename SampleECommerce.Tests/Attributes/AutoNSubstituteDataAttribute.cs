e90ec4dc2865f6680edb804d611873e16d21411a
Rex Ng <rexcfnghk@gmail.com>
2024-07-20 12:54:52 +0800

    Add unit test project

---
+++ SampleECommerce.Tests/Attributes/AutoNSubstituteDataAttribute.cs
@@ -0,0 +1,9 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+
+namespace SampleECommerce.Tests.Attributes;
+
+public sealed class AutoNSubstituteDataAttribute() : AutoDataAttribute(
+    () => new Fixture()
+        .Customize(new AutoNSubstituteCustomization()));
