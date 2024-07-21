3a61703887db224c5224d7854837bee5ec360da3
Rex Ng <rexcfnghk@gmail.com>
2024-07-21 21:55:26 +0800

    Remove useless table

--- assets/seed-scripts.sql
+++ assets/seed-scripts.sql
@@ -7,15 +7,9 @@ CREATE TABLE Users (
     UserName NVARCHAR(50) NOT NULL,
     EncryptedPassword VARBINARY(128) NOT NULL,
     Salt VARBINARY(16) NOT NULL,
-
-    CONSTRAINT AK_UserNameId UNIQUE(UserName)
-)
-
-CREATE TABLE UserBalances (
-    UserId INT NOT NULL PRIMARY KEY,
     Balance SMALLMONEY NOT NULL DEFAULT (100.00)
 
-    FOREIGN KEY UserId REFERENCES Users(Id)
+    CONSTRAINT AK_UserNameId UNIQUE(UserName)
 )
 
 GO
